b4840401cf5987a88f6bdcbbb71146a4151b839d
FernandoM <fmillan.js@gmail.com>
2016-12-31 04:46:25 -0700

    working on header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import 'hammerjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app works!';
+  // myColor = 'primary';
 }
