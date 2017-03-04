ecc80176fc986de906166d8baf5f6ccac5d9abd3
FernandoM <fmillan.js@gmail.com>
2017-03-03 20:25:46 -0700

    Added logo initial animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, trigger, state, transition, animate, style } from '@angular/core';
+import { Component, Input, trigger, state, transition, animate, keyframes, style } from '@angular/core';
 
 @Component({
   selector: 'fm-root',
@@ -6,15 +6,44 @@ import { Component, Input, trigger, state, transition, animate, style } from '@a
   styleUrls: ['./app.component.scss'],
   animations: [
     trigger('loading', [
-      transition('void => *', [
-        style({opacity: '0'}),
-        animate(1000)
+      transition(':enter', [
+        animate('2s', keyframes([
+          style({
+            opacity: 0,
+            transform: 'scale(1) translate(-50%, -50%)',
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            width: '50%',
+            offset: 0
+          }),
+          style({
+            opacity: 1,
+            transform: 'scale(1.2) translate(-50%, -50%)',
+            top: '50%',
+            left: '50%',
+            width: '50%',
+            offset: 0.4
+          }),
+          style({
+            opacity: 1,
+            transform: 'scale(1) translate(0%, 0%)',
+            top: '0%',
+            left: '0%',
+            width: '10%',
+            offset: 0.8
+          }),
+          style({
+            opacity: 1,
+            transform: 'scale(1) translate(0%, 0%)',
+            width: '10%',
+            top: '0%',
+            left: '0%',
+            offset: 1
+          })
+        ]))
       ]),
-      transition('* => void', [
-        style({opacity: '1'}),
-        animate(500)
-      ])
-    ]),
+    ])
   ]
 })
 export class AppComponent {
