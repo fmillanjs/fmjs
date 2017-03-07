cfba8464f5a380c4dcc5dce2b703b89b9603d8cc
FernandoM <fmillan.js@gmail.com>
2017-03-07 07:57:15 -0700

    started profile page

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -90,8 +90,8 @@ export function routeTransition() {
 
 function slideLeft() {
   return trigger('routeTransition', [
-    state('void', style({position: 'fixed', width: '100%'}) ),
-    state('*', style({position: 'fixed', width: '100%'}) ),
+    state('void', style({position: 'absolute', width: '100%'}) ),
+    state('*', style({position: 'absolute', width: '100%'}) ),
     transition(':enter', [
       style({transform: 'translateX(100%)'}),
       animate('0.5s ease-in-out', style({transform: 'translateX(0%)'}))
