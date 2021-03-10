ad8c256152e082ddfd2f3985364e0bae6e250b28
fmillanjs <fmillan.js@gmail.com>
2021-03-10 13:19:34 -0800

    restart portfolio

--- src/environments/firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    // Initialize Firebase configurations
-    apiKey: "AIzaSyDOIEZ9RwIp8LumZ4__aHtMOjpg7sdf-EI",
-    authDomain: "fmjs-2ef79.firebaseapp.com",
-    databaseURL: "https://fmjs-2ef79.firebaseio.com",
-    storageBucket: "fmjs-2ef79.appspot.com",
-    messagingSenderId: "341785476869"
-}
